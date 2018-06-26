0ecea849124fd844f350211f988a66d6c6b31014
cdmazhicheng <260105903@qq.com>
2018-06-26 17:27:29 +0800

    结束基础教程

--- mazc/polls/views.py
+++ mazc/polls/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import render, get_object_or_404
-from django.http import HttpResponse, Http404
-from .models import Question
+from django.http import HttpResponse, Http404, HttpResponseRedirect
+from .models import Question, Choice
 from django.template import loader
+from django.urls import reverse
+from django.views import generic
 
 # def first_page(self):
 #     return HttpResponse("测试...")
@@ -23,11 +25,44 @@ from django.template import loader
 #     question = get_object_or_404(Question, pk=question_id)
 #     return render(self, 'polls/detail.html', {'question': question})
 
-def index(request):
-    latest_question_list = Question.objects.all
-    return render(request, 'polls/index.html', {'latest_question_list': latest_question_list})
+# def index(request):
+#     latest_question_list = Question.objects.all
+#     return render(request, 'polls/index.html', {'latest_question_list': latest_question_list})
+#
+# def detail(request, question_id):
+#     question = get_object_or_404(Question, pk=question_id)
+#     return render(request, 'polls/detail.html', {'question': question})
 
-def detail(request, question_id):
+class IndexView(generic.ListView):
+    template_name = 'polls/index.html'
+    context_object_name = 'latest_question_list'
+
+    def get_queryset(self):
+        """Return the last five published questions."""
+        return Question.objects.order_by('-pub_date')[:5]
+
+
+class DetailView(generic.DetailView):
+    model = Question
+    template_name = 'polls/detail.html'
+
+
+class ResultsView(generic.DetailView):
+    model = Question
+    template_name = 'polls/results.html'
+
+def vote(request, question_id):
     question = get_object_or_404(Question, pk=question_id)
-    return render(request, 'polls/detail.html', {'question': question})
+    try:
+        selected_choice = question.choice_set.get(pk=request.POST['choice'])
+    except (KeyError, Choice.DoesNotExist):
+        return render(request, 'polls/detail.html', { 'question': question, 'error_message': "You didn't select a choice.", })
+    else:
+        selected_choice.votes += 1
+        selected_choice.save()
+        return HttpResponseRedirect(reverse('polls:results', args=(question.id,)))
+
+# def results(request, question_id):
+#     question = get_object_or_404(Question, pk=question_id)
+#     return render(request, 'polls/results.html', {'question': question})
 
