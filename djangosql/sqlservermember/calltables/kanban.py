e7c644ae067e87e8efbd85c55635422d2789a152
cdmazhicheng <260105903@qq.com>
2018-07-05 17:48:57 +0800

    增加了会员模块的支付会员数量用例

---
+++ djangosql/sqlservermember/calltables/kanban.py
@@ -0,0 +1,119 @@
+import pymssql
+
+conn = pymssql.connect(host='192.168.5.32', user='test', password='85442791', database='Ytny')
+cursor = conn.cursor()
+begin_time = '2018-04-04'
+end_time = '2018-04-05'
+
+#######注册用户模块##########
+
+# 时间区间中的注册人员
+def pit_member_user_register(self):
+
+    sql_pit_member_user_register = "select COUNT(*) from pit_member_user " \
+                          "where register_time > = '{} 00:00:00.000' and register_time < = '{} 00:00:00.000'".format(begin_time, end_time)
+    cursor.execute(sql_pit_member_user_register)
+    rs_sql_pit_member_user_register= cursor.fetchall()
+
+    return rs_sql_pit_member_user_register
+# 当日注册人员
+def pit_member_user_register_today(self):
+
+    sql_pit_member_user_register_today = "select COUNT(*) from pit_member_user where DateDiff(dd,register_time,getdate())=0"
+    cursor.execute(sql_pit_member_user_register_today)
+    rs_sql_pit_member_user_register_today= cursor.fetchall()
+
+    return rs_sql_pit_member_user_register_today
+# 昨日注册人员
+def pit_member_user_register_yesterday(self):
+
+    sql_pit_member_user_register_yesterday = "select COUNT(*) from pit_member_user where DateDiff(dd,register_time,getdate())=1"
+    cursor.execute(sql_pit_member_user_register_yesterday)
+    rs_sql_pit_member_user_register_yesterday= cursor.fetchall()
+
+    return rs_sql_pit_member_user_register_yesterday
+
+
+
+#######新增会员模块##########
+
+#新增会员数量
+def pit_member_user_vip(self):
+    sql_pit_member_user_vip = "select COUNT(*) from pit_member_finance " \
+                              "inner join pit_member_user " \
+                              "on pit_member_user.id = pit_member_finance.id " \
+                              "where pit_member_user.register_time > = '{} 00:00:00.000' " \
+                              "and pit_member_user.register_time < = '{} 00:00:00.000' " \
+                              "and pit_member_finance.growth_level>0".format(begin_time, end_time)
+    cursor.execute(sql_pit_member_user_vip)
+    rs_sql_pit_member_user_vip= cursor.fetchall()
+
+    return rs_sql_pit_member_user_vip
+# 当日新增会员
+def pit_member_user_vip_today(self):
+    sql_pit_member_user_vip_today = "select COUNT(*) from pit_member_finance " \
+                                    "inner join pit_member_user " \
+                                    "on pit_member_user.id = pit_member_finance.id " \
+                                    "where DateDiff(dd,pit_member_user.register_time,getdate())=0 " \
+                                    "and pit_member_finance.growth_level>0"
+    cursor.execute(sql_pit_member_user_vip_today)
+    rs_sql_pit_member_user_vip_today= cursor.fetchall()
+
+    return rs_sql_pit_member_user_vip_today
+#  昨日新增会员
+def pit_member_user_vip_yesterday(self):
+    sql_pit_member_user_vip_yesterday = "select COUNT(*) from pit_member_finance " \
+                                    "inner join pit_member_user " \
+                                    "on pit_member_user.id = pit_member_finance.id " \
+                                    "where DateDiff(dd,pit_member_user.register_time,getdate())=0 " \
+                                    "and pit_member_finance.growth_level=1"
+    cursor.execute(sql_pit_member_user_vip_yesterday)
+    rs_sql_pit_member_user_vip_yesterday= cursor.fetchall()
+
+    return rs_sql_pit_member_user_vip_yesterday
+
+
+#######支付人数##########
+
+#  选取时间段的支付人数
+def pit_member_user_pay_number(self):
+    sql_pit_member_user_pay_number = "select pit_oil_order.user_id,COUNT(*) from pit_member_finance " \
+                                 "inner join pit_member_user " \
+                                 "on pit_member_user.id = pit_member_finance.id " \
+                                 "inner join pit_oil_order " \
+                                 "on pit_member_user.id = pit_oil_order.user_id " \
+                                 "where pit_oil_order.pay_time > = '{} 00:00:00.000' " \
+                                 "and pit_oil_order.pay_time < = '{} 00:00:00.000' " \
+                                 "and pit_member_finance.growth_level>0 group by user_id ".format(begin_time, end_time)
+    cursor.execute(sql_pit_member_user_pay_number)
+    rs_sql_pit_member_user_user_pay_number= cursor.fetchall()
+
+    return rs_sql_pit_member_user_user_pay_number
+
+#  今日支付人数
+def pit_member_user_pay_number_today(self):
+    sql_pit_member_user_pay_number_today = "select pit_oil_order.user_id,COUNT(*) from pit_member_finance " \
+                                           "inner join pit_member_user " \
+                                           "on pit_member_user.id = pit_member_finance.id " \
+                                           "inner join pit_oil_order " \
+                                           "on pit_member_user.id = pit_oil_order.user_id " \
+                                           "where DateDiff(dd,pit_oil_order.pay_time,getdate())=0 " \
+                                           "and pit_member_finance.growth_level>0 group by user_id".format(begin_time, end_time)
+    cursor.execute(sql_pit_member_user_pay_number_today)
+    rs_sql_pit_member_user_user_pay_number_today= cursor.fetchall()
+
+    return rs_sql_pit_member_user_user_pay_number_today
+
+#  昨日支付人数
+def pit_member_user_pay_number_yesterday(self):
+    sql_pit_member_user_pay_number_yesterday = "select pit_oil_order.user_id,COUNT(*) from pit_member_finance " \
+                                           "inner join pit_member_user " \
+                                           "on pit_member_user.id = pit_member_finance.id " \
+                                           "inner join pit_oil_order " \
+                                           "on pit_member_user.id = pit_oil_order.user_id " \
+                                           "where DateDiff(dd,pit_oil_order.pay_time,getdate())=1 " \
+                                           "and pit_member_finance.growth_level>0 group by user_id".format(begin_time, end_time)
+    cursor.execute(sql_pit_member_user_pay_number_yesterday)
+    rs_sql_pit_member_user_user_pay_number_yesterday = cursor.fetchall()
+
+    return rs_sql_pit_member_user_user_pay_number_yesterday
--- djangosql/sqlservermember/views.py
+++ djangosql/sqlservermember/views.py
@@ -1,15 +1,102 @@
 from django.shortcuts import render
-from sqlserveruser.calltables import user_table, coupon_table, order_table
+from sqlservermember.calltables import kanban
 import time
 from . import models
 from django.http import HttpResponse
 from sqlserveruser import models
 
-#账号数据
 def member(request):
 
+#######注册用户##########
+
+    #时间区间注册数据
+    pit_member_user_register = kanban.pit_member_user_register(request)[0][0]
+    begin_time = kanban.begin_time
+    end_time = kanban.end_time
+    # #当天注册数据
+    # pit_member_user_register_today = 3
+    # #昨日注册数据
+    # pit_member_user_register_yesterday = 5
+    #当天注册数据
+    pit_member_user_register_today = kanban.pit_member_user_register_today(request)[0][0]
+    #昨日注册数据
+    pit_member_user_register_yesterday = kanban.pit_member_user_register_yesterday(request)[0][0]
+    #注册人数同比增长
+    if pit_member_user_register_yesterday == 0:
+        register_ratio = pit_member_user_register_today
+    elif pit_member_user_register_today == 0:
+        register_ratio = -pit_member_user_register_yesterday
+    else:
+        register_ratio = pit_member_user_register_today/pit_member_user_register_yesterday
+
+#######新增会员##########
+
+    # 新增会员数量
+    pit_member_user_vip = kanban.pit_member_user_vip(request)[0][0]
+    #今日会员数量
+    pit_member_user_vip_today = kanban.pit_member_user_vip_today(request)[0][0]
+    #昨日会员数量
+    pit_member_user_vip_yesterday = kanban.pit_member_user_vip_yesterday(request)[0][0]
+    #会员同比增长
+    if pit_member_user_vip_today == 0:
+        vip_ratio = pit_member_user_vip_today
+    elif pit_member_user_vip_yesterday == 0:
+        vip_ratio = -pit_member_user_vip_yesterday
+    else:
+        vip_ratio = pit_member_user_vip_today / pit_member_user_vip_yesterday
+
+#######会员支付人数##########
+
+        #时间区间会员支付人数
+        pit_member_user_pay_number = len(kanban.pit_member_user_pay_number(request))
+        #今日会员支付人数
+        pit_member_user_pay_number_today = len(kanban.pit_member_user_pay_number_today(request))
+        #昨日会员支付人数
+        pit_member_user_pay_number_yesterday = len(kanban.pit_member_user_pay_number_yesterday(request))
+        #会员同比增长
+        if pit_member_user_pay_number_today == 0:
+            pay_number_ratio = pit_member_user_pay_number_today
+        elif pit_member_user_pay_number_yesterday == 0:
+            pay_number_ratio = -pit_member_user_pay_number_yesterday
+        else:
+            pay_number_ratio = round(pit_member_user_pay_number_today / pit_member_user_pay_number_yesterday, 2)
+
+
+####################
+##  context部分 ####
+####################
+
+
     context = {
-               #本地数据库账号
+
+#######注册用户##########
+
+               #时间区间注册数据
+                'pit_member_user_register': pit_member_user_register,
+                'begin_time': begin_time,
+                'end_time': end_time,
+                #当天注册数据
+                'pit_member_user_register_today': pit_member_user_register_today,
+                # 昨日注册数据
+                'pit_member_user_register_yesterday': pit_member_user_register_yesterday,
+                # 注册人数同比增长
+                'register_ratio': register_ratio,
+
+#######新增会员##########
+
+                # 新增会员数量
+                'pit_member_user_vip': pit_member_user_vip,
+                'pit_member_user_vip_today': pit_member_user_vip_today,
+                'pit_member_user_vip_yesterday': pit_member_user_vip_yesterday,
+                'vip_ratio': vip_ratio,
+
+#######会员支付人数##########
+
+                'pit_member_user_pay_number': pit_member_user_pay_number,
+                'pit_member_user_pay_number_today': pit_member_user_pay_number_today,
+                'pit_member_user_pay_number_yesterday': pit_member_user_pay_number_yesterday,
+                'pay_number_ratio': pay_number_ratio,
+
                }
 
     return render(request, 'sqlservertester/member.html', context)
